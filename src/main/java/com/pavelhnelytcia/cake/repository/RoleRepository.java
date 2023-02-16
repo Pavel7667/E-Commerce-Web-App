92356045036d6baa9e28b31a3070abc3c726e1fc
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-02-16 13:37:04 +0200

    add @Entity UserRepository and RoleRepository  for Spring-Security configuration

---
+++ src/main/java/com/pavelhnelytcia/cake/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.pavelhnelytcia.cake.repository;
+
+import com.pavelhnelytcia.cake.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+}
