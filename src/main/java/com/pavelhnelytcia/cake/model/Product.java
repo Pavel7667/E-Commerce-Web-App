83010a876a072b4765074d80d5ac8dbfa3fb6a03
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-02-16 13:19:27 +0200

    add to @Entity for Product

---
+++ src/main/java/com/pavelhnelytcia/cake/model/Product.java
@@ -0,0 +1,31 @@
+package com.pavelhnelytcia.cake.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Entity Product combine with Entity Category via id field
+ */
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+}
